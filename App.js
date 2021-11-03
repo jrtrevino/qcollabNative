0ef700e4890453a2f23625a68c9a2a1cce522bec
jrtrevino <j.trevino95@icloud.com>
2021-11-02 23:13:15 -0700

    Modifed app.js style

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {useState} from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import LoginScreen from './components/LoginScreen';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  return <LoginScreen />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <LoginScreen />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2F303A',
+  },
+});
 
 export default App;
